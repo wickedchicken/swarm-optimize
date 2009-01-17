e48c34d236fef85a676a375ff54ff2118f129a4f
Mike Stipicevic <stipim@rpi.edu>
2009-01-17 18:09:03 -0500

    Added in basic cost function, fixed small error

--- vector.rb
+++ vector.rb
@@ -1,7 +1,18 @@
 class Swarm
-	def initialize(size=50,range = {:min => 0.0, :max => 1.0})
+	# redo to be hash set
+	def initialize(size=50,
+		       range = {:min => 0.0,
+		       :max => 1.0},
+		       window = 3,
+		       nrg_func = {|slice|
+				   # breaks down if window < 3
+				   idx = slice.length/2
+				   [idx,idx+1].map({|x| slice[x] - slice[x-1]}).inject {|accum,obj| accum + obj*obj}
+				  })
 		run = range[:min] - range[:max]
-		@arry = Array.new(size) {|x| (rand * range[:max]) - range[:min] }
+		@nrg = nrg_func
+		@window = 3
+		@arry = Array.new(size) {|x| (rand * run - range[:min] }
 	end
 
 	def arry
