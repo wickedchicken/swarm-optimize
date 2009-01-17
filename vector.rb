fc9b6e65b5d79b937fbec5e826995e35fd54b15b
Mike Stipicevic <stipim@rpi.edu>
2009-01-17 17:34:22 -0500

    now compiles

--- vector.rb
+++ vector.rb
@@ -1,5 +1,5 @@
-class optarry
-	def initialize(size=50,range = {:min => 0.0, :max => 1.0}])
+class Swarm
+	def initialize(size=50,range = {:min => 0.0, :max => 1.0})
 		run = range[:min] - range[:max]
 		@arry = Array.new(size) {|x| (rand * range[:max]) - range[:min] }
 	end
