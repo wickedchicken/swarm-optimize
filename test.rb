2140f258f37c6584b9616706aae3fc2fa7d35a48
Mike Stipicevic <stipim@rpi.edu>
2009-01-18 20:37:47 -0500

    Added a training test

--- test.rb
+++ test.rb
@@ -3,6 +3,11 @@ load 'vector.rb'
 
 class Creation < Test::Unit::TestCase
 	def test_default
-		assert Swarm.new
+		assert(Swarm.new,'could not create default Swarm object')
+	end
+
+	def test_nrg_default
+		swarm = Swarm.new
+		assert(swarm.calc_nrg,'could not calculate energy')
 	end
 end
