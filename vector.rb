addcc33d95996f6fba5222b7d017304391ac5ae4
Mike Stipicevic <stipim@rpi.edu>
2009-01-19 09:09:30 -0500

    Fixed weird error caused by using each_slice

--- vector.rb
+++ vector.rb
@@ -7,16 +7,14 @@ class Swarm
 	class BoundedArray < Array
 		def [](idx)
 			return self.at(idx) unless ((idx < 0) or (idx > self.length))
-			return 0.0
+			if((idx < 0)) then
+				return [self.at(0).at(0),0.0]
+			else
+				return [self.last.at(0),0.0]
+			end
+
 		end
 	end
-	@nrg = lambda { |slice|
-		# breaks down if window < 3
-		# should be odd...
-		idx = slice.length/2
-		[idx,idx+1].map {|x| slice[x] - slice[x-1]}.inject {|accum,obj| accum + obj*obj }
-	}
-
 #	@trainfunc = { |rate,slice|
 #		# breaks down if window < 3
 #		# should be odd...
@@ -26,15 +24,22 @@ class Swarm
 #		slice.length.times.map {|x|
 			
 	def calc_nrg
-		side = @win/2
+		side = (@win/2)
 		# some inefficiencies
-		((-side+1)..(@arry.length+side-2)).each_slice(@win) {|indices|
+		slices = (-side..@arry.length-1-side).to_a.map {|i|
+			(i..i+@win-1).to_a
+		}
+
+		slices.each {|indices|
 			idx = indices[side]
-			@arry[idx][1] = @nrg.call(indices.map {|x| @arry[x][0]})
+
+			idxes = indices.map {|x| @arry[x][0]}
+
+			@arry[idx][1] = @nrg.call(idxes)
 		}
 
 		@arry.each {|x|
-			puts x[0] + "\t" + x[1]
+			puts x[0].to_s + "\t" + x[1].to_s
 		}
 	end
 
@@ -51,7 +56,15 @@ class Swarm
 		@nrg = nrg_func unless nrg_func.nil?
 		@win = window
 		@arry = BoundedArray.new(size) {|x| [(rand * run - range[:min]) , 0.0]}
+		@nrg = lambda { |slice|
+			# breaks down if window < 3
+			# should be odd...
 		
+			idx = slice.length/2
+			[idx,idx+1].map {|x| slice[x] - slice[x-1]}.inject {|accum,obj| accum + obj*obj }
+		}
+
+
 
 		raise "window size less than swarm size" unless @arry.length > @win
 		raise "window size less than three" unless @win >= 3
